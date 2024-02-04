35adb650c9e93587ce2151e6657aaf79c4c63699
Swetha <swethamag.2000@gmail.com>
2024-02-04 14:53:28 +0530

    forward

--- peer.py
+++ peer.py
@@ -12,18 +12,6 @@ class Peer:
     def __str__(self):
         return str(self.id) + " " + str(self.slow) + " " + str(self.slowcpu)+ " " + str(self.hashpower)
     
-    def forward_transaction(self, transaction, sender_peer, env):
-        # all adjacent peers except sender
-        for adj in G.adj[self.id]:
-            if adj != sender_peer.id:
-                # send transaction to adj peer
-                peers[adj].forward_transaction(transaction, self, env)
-                yield env.timeout(transaction.generate_qdelay(links[self.id][adj]))
-                pass
-
-
-
-
 
     def receive_transaction(self, transaction):
         trans_ids = [x.id for x in self.transactions_queue]
@@ -39,3 +27,13 @@ class Link:
 
     def __str__(self):
         return str(self.i) + " " + str(self.j) + " " + str(self.cij) + " " + str(self.roij)
+    
+
+def forward_transaction(peer, transaction, sender_peer, env):
+        # all adjacent peers except sender
+        for adj in G.adj[peer.id]:
+            if adj != sender_peer.id:
+                # send transaction to adj peer
+                peers[adj].forward_transaction(transaction, peer, env)
+                yield env.timeout(transaction.generate_qdelay(links[peer.id][adj]))
+                pass
