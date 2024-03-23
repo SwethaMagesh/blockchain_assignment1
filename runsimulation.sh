a000e5ec79876585f727360e8711025f82a30032
chaitragurjar <chaitra.gurjar@gmail.com>
2024-03-23 15:11:47 +0530

    parameters

--- runsimulation.sh
+++ runsimulation.sh
@@ -9,7 +9,8 @@ while [[ "$#" -gt 0 ]]
     case $1 in
       -n|--peers) n="$2"; shift;;
       -z0|--slow) z0="$2"; shift;;
-      -z1|--low) z1="$2"; shift;;
+      -a1|--zeta1) a1="$2"; shift;;
+      -a2|--zet2) a2="$2"; shift;;
       -Ttx|--txninterval) Ttx="$2"; shift;;
       -I|--blockinterval) I="$2"; shift;;
     esac
@@ -17,4 +18,4 @@ while [[ "$#" -gt 0 ]]
 done
 
 cd src
-python3 main.py -n $n -z0 $z0 -z1 $z1 -Ttx $Ttx -I $I -a1 0.3 -a2 0.3
+python3 main.py -n $n -z0 $z0 -a1 $a1 -a2 $a2 -Ttx $Ttx -I $I
